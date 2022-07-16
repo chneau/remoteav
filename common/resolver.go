c9bec5c31c43f26c3fef6a7c194376e59d169194
chneau <charles63500@gmail.com>
2022-07-16 01:47:47 +0100

    ok

--- common/resolver.go
+++ common/resolver.go
@@ -1,13 +1,14 @@
 package common
 
 import (
-	"log"
-
+	"github.com/blackjack/webcam"
 	"github.com/chneau/remoteav/camera"
+	"github.com/samber/lo"
 )
 
 type Resolver struct {
 	cameras        []*camera.Camera
+	camera         *camera.Camera
 	selectedCamera *SelectedCamera
 }
 
@@ -23,8 +24,30 @@ type SelectedCamera struct {
 
 func (r *Resolver) SetSelectedCamera(args *SelectedCamera) bool {
 	r.selectedCamera = args
-	log.Printf("args: %#+v\n", args)
-	return true
+	if r.camera != nil {
+		_ = r.camera.StopStreaming()
+		r.camera = nil
+	}
+	for _, camera := range r.cameras {
+		if camera.Id() == args.Id {
+			r.camera = camera
+		}
+	}
+	if r.camera == nil {
+		return false
+	}
+	pixelFormat, found := lo.FindKey(r.camera.GetSupportedFormats(), args.Format)
+	if !found {
+		return false
+	}
+	frameSize, found := lo.Find(r.camera.GetSupportedFrameSizes(pixelFormat), func(frameSize webcam.FrameSize) bool {
+		return frameSize.GetString() == args.FrameSize
+	})
+	if !found {
+		return false
+	}
+	_, _, _, err := r.camera.SetImageFormat(pixelFormat, frameSize.MaxWidth, frameSize.MaxHeight)
+	return err == nil
 }
 
 func NewResolver(cameras []*camera.Camera) *Resolver {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,18 @@ export const App = () => {
   const { data } = useGetAllCamerasQuery();
   const [selectedCamera, setSelectedCamera] = useState<SetSelectedCameraMutationVariables>();
   const [setSelectedCameraMutation] = useSetSelectedCameraMutation();
+  const [isSuccess, setIsSuccess] = useState<boolean | undefined>(undefined);
   useEffect(() => {
     if (!selectedCamera) return;
-    setSelectedCameraMutation({ variables: selectedCamera });
+    (async () => {
+      setIsSuccess(undefined);
+      const result = await setSelectedCameraMutation({ variables: selectedCamera });
+      setIsSuccess(result.data?.setSelectedCamera);
+    })();
   }, [selectedCamera]);
   return (
     <>
-      <h1>Cameras {selectedCamera && `(${selectedCamera.id} ${selectedCamera.format} ${selectedCamera.frameSize})`}</h1>
+      <h1>Cameras {selectedCamera && `(${selectedCamera.id} ${selectedCamera.format} ${selectedCamera.frameSize}) => ${isSuccess}`}</h1>
       {data?.cameras.map(({ id, supportedFormats }) =>
         supportedFormats.map(({ format, frameSizes }) =>
           frameSizes.map((frameSize, i) => (
