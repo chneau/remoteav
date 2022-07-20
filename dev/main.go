525a84361d838344c9766871402857695aad135e
chneau <charles63500@gmail.com>
2022-07-21 00:15:43 +0100

    proxy

--- dev/main.go
+++ dev/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/chneau/remoteav/av"
 	"github.com/chneau/remoteav/common"
@@ -25,7 +27,9 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.With(middleware.Logger).Handle("/graphql", &relay.Handler{Schema: schema})
 	router.Get("/graphiql", graphiqlHandler)
-	router.Get("/*", proxy)
+	url := lo.Must(url.Parse("http://localhost:5173"))
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	router.Get("/*", proxy.ServeHTTP)
 	router.Get("/stream", common.StreamHandlerfunc(resolver.ImageStream()))
 	fmt.Println("Listening on port http://localhost:7777")
 	lo.Must0(http.ListenAndServe(":7777", router))
